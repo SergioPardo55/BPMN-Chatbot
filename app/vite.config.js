290f4a438ad09c94ab778fd193ffd0e552751565
SergioPardo55 <s.pardog@uniandes.edu.co>
2025-06-05 17:18:45 +0200

    Changing to Vercel

--- app/vite.config.js
+++ app/vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/BPMN-Chatbot",
   plugins: [react()],
+  build: {
+    outDir: 'dist', 
+    sourcemap: false, 
+  },
 })
